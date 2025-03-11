b4f090595908e2a070b71b08596fdaa2f2f7eb24
TAMANNA <122337651+tamu1704@users.noreply.github.com>
2025-03-11 11:50:10 +0530

    Add files via upload

---
+++ Tamanna11March2025ClassTest.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+//Find the Sum of Digits of a Number (123 → 6)
+public class Tamanna11March2025ClassTest {
+    public static int res(int n){
+        int sum=0;
+        while(n>0){
+            int temp=n%10;
+            sum=sum+temp;
+            n=n/10;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        int r=res(n);
+        System.out.println(r);
+    }
+}
