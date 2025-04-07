dfee4acd03904d1956cd89b6da4f910d77dcb01a
TAMANNA <122337651+tamu1704@users.noreply.github.com>
2025-04-07 11:46:34 +0530

    Add files via upload

--- leetcode560.java
+++ leetcode560.java
@@ -1,24 +1,24 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class leetcode560 {
-    public static int result(int[] a,int k){
-        Map<Integer,Integer>m=new HashMap<>();
-        m.put(0,1);
-        int preSum=0, c=0;
-        for(int n:a){
-            preSum+=n;
-            if(m.containsKey(preSum-k)){
-                c+=m.get(preSum-k);
+import java.util.*;
+public class leetcode560{
+    //given an array of integers 
+    public static int res(int[] a,int k){
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        hm.put(0,1);  //to count subarray that sum upto k from start
+        int c=0,sum=0;
+        for(int i:a){
+            sum+=i;
+            //if (sum-k) exists, we found subarray that ends at current index and sums to k
+            if(hm.containsKey(sum-k)){
+                c+=hm.get(sum-k);
             }
-            m.put(preSum,m.getOrDefault(preSum, 0)+1);
+            hm.put(sum,hm.getOrDefault(sum,0)+1);
         }
         return c;
     }
-
-    public static void main(String[] args) {
-        int[] a = { 1, 2, 3 };
-        int k = 3; // 2
-        System.out.println(result(a, k));
+    public static void main(String[] args){
+        int[] a={1,2,3};
+        int k=3;
+        int r=res(a,k);
+        System.out.println(k+" "+r);
     }
 }
